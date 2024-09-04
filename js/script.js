42ed00ff148abbc90bee755cb9c7a47eaa83f42d
Bram Bolt <bram.bolt@icloud.com>
2024-09-04 11:28:08 +0200

    added js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function convertUrl() {
+    // Get the input URL
+    const inputUrl = document.getElementById('inputUrl').value;
+
+    // Encode the URL to Base64
+    const encodedUrl = btoa(inputUrl);
+
+    // Create the final URL
+    const finalUrl = 'https://www.koalastothemax.com/?' + encodedUrl;
+
+    // Update the link text and href attribute
+    const outputLink = document.getElementById('outputLink');
+    outputLink.innerText = finalUrl;
+    outputLink.href = finalUrl;
+}
+
